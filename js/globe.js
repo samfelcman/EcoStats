6d18daeddd18f1c41cbd956b9abe5821ef1fabfb
Samuel Felcman <samuelfelcman17@gmail.com>
2026-08-18 20:22:30 -0300

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -23,11 +23,18 @@ window.addEventListener('resize', () => {
 fetch('https://unpkg.com/world-atlas/countries-110m.json')
   .then(r => r.json())
   .then(data => {
-    const countries = topojson.feature(data, data.objects.countries).fetatures;
+    console.log('World Atlas carregada:', data);
+    const countries = topojson.feature(data, data.objects.countries).features;
+
+    console.log(
+      'Países carregados:',
+      countries.length
+    );
+
     world
-    .polygonData(countries)
-    .polygonAltitude(0.01)
-    .polygonCapColor(d => d === currentHover ? 'rgba(56,189,248,0.35' : 'rgba(255,255,255,0.09')
+    .polygonsData(countries)
+    .polygonsAltitude(0.01)
+    .polygonCapColor(d => d === currentHover ? 'rgba(56,189,248,0.35)' : 'rgba(255,255,255,0.09)')
     .polygonSideColor(() => 'rgba(14,165,233,0.12)')
     .polygonStrokeColor(() => 'rgba(99,179,237,0.3)')
     .polygonLabel(d => `<div style="font-family:'DM Mono',monospace; font-size:11px; background:rgba(12,17,32,0.9); border:1px solid rgba(99,179,237,0.2); border-radius:6px; padding:5px 10px; color:#e2e8f0;">${d.properties.name || '—'}</div>`)
@@ -40,7 +47,7 @@ fetch('https://unpkg.com/world-atlas/countries-110m.json')
         console.log("País clicado:", polygon);
         const properties = polygon.properties || {};
 
-        const isoCode = 
+        const isocode = 
           properties.ISO_A3 ||
           properties.ISO3 ||
           properties.ADM0_A3 ||
@@ -69,7 +76,6 @@ fetch('https://unpkg.com/world-atlas/countries-110m.json')
           isoCode,
           countryName
         );
-
       });
   })
   // Fly to point of view (Go to the country you click on)
