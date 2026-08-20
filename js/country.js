9710875921f2bc49cc5726ad2ae408864b02ddbe
Samuel Felcman <samuelfelcman17@gmail.com>
2026-08-19 21:26:21 -0300

    Update country.js

--- js/country.js
+++ js/country.js
@@ -1,7 +1,11 @@
-const REST_COUNTRIES_API_KEY = 'rc_live_52df4e65d2cc49b1a9fc427db434210a';
-const REST_COUNTRIES_BASE_URL = 'https://api.restcountries.com/countries/v5';
+fetch(
+  'https://api.restcountries.com/countries/v5?q=canada',
+  { headers: { 'Authorization': 'Bearer rc_live_52df4e65d2cc49b1a9fc427db434210a' } }
+)
+  .then(function (response) { return response.json(); })
+  .then(function (data) { console.log(data); });
+  
 const WORLD_BANK_BASE_URL = 'https://api.worldbank.org/v2/country';
-
 const COUNTRY_RESPONSE_FIELDS = [
   'names.common',
   'population',
